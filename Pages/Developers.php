09d62f55a8e2301559ee18daffe791fb40c5c895
Polca13 <72781747+Polca13@users.noreply.github.com>
2021-10-19 17:26:30 +0800

    Add files via upload

---
+++ Pages/Developers.php
@@ -0,0 +1,34 @@
+<div class="developer">
+ <div class="left">
+   
+</div>
+ <div class="right">
+ <div class="profilebox">
+       <div class="imagebox">
+            <img src="img/Claire.jpg" >
+        </div>
+        <h2>Claire C. Casinillo<br><span>Project Manager <br><h5>"In every failure, there's always a success. In every achievements, there's always courage and trust. Don't lose hope, indeed."</h5></span> </h2>
+    </div>
+    <div class="profilebox">
+       <div class="imagebox">
+            <img src="img/dave.jpg" >
+        </div>
+        <h2>Dave E. Baluya<br><span>Technical Writer</span> </h2>
+        <h5></h5>
+    </div>
+    <div class="profilebox">
+       <div class="imagebox">
+            <img src="img/PG.jpg" >
+        </div>
+        <h2>Paul Gerald Cabangal<br><span>Programmer/Technical Writer</span> </h2>
+        <h5></h5>
+    </div>
+    <div class="profilebox">
+       <div class="imagebox">
+            <img src="img/mae.jpg" >
+        </div>
+        <h2>Erica Mae B. Obando<br><span>Programmer<br><h5> "A man without self-control is like a city broken into and left without walls." -Proverbs 25:28</h5></span></h2>
+        
+    </div>
+ </div>
+</div>

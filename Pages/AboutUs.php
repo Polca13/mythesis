e90912650c80f05e9aaaec6e60c6acae1ce32092
Polca13 <72781747+Polca13@users.noreply.github.com>
2021-10-19 17:24:33 +0800

    Add files via upload

---
+++ Pages/AboutUs.php
@@ -0,0 +1,15 @@
+<div class="aboutus">
+    <div class="card mb-3 h-100">
+    <img src="img/head.jpg" class="card img" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">About Us</h5>
+        <p class="card-text">This is COC family, we are a group of 4th year students from Eastern Visayas State University- Ormoc City Campus. Our group began when our
+            instructor in our capstone subject wants us to form a group of 3 people. Shortly after the announcement of groupings all our classmates already has a team but we were left out with a member of 2 each group so
+            we decided to form a group and left 1 member out to create a team with 3 people, we were the last one to submit a group member beacuse we dont know whose going to be out but one of our member sacrificed just so we can submit our 
+            team list ontime, but fate brings us together as our adviser decides to let as all be a team and now we are ready to face all the hardships and trials we have just so we could pass our subjects and graduate together.
+        </p>
+    </div>
+    </div>
+
+
+</div>

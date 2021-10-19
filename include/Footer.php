cb390e56d06c60ed0426b4a65d1c3eb987b22598
Polca13 <72781747+Polca13@users.noreply.github.com>
2021-10-19 17:34:01 +0800

    Add files via upload

---
+++ include/Footer.php
@@ -0,0 +1,50 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+ <footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="footer p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><span class="icon"><i class="fab fa-facebook-f"></i
+      ></span></a>
+
+      <!-- Twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><span class="icon"><i class="fab fa-twitter"></i
+      ></span></a>
+
+      <!-- Google -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><span class="icon"><i class="fab fa-google"></i
+      ></span></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><span class="icon"><i class="fab fa-instagram"></i
+      ></span></a>
+
+      <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><span class="icon"><i class="fab fa-linkedin-in"></i
+      ></span></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><span class="icon"><i class="fab fa-github"></i
+      ></span></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Copyright:
+    <a class="text-white" href="#">COCFamily.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+  
+
+<!-- End of .container -->

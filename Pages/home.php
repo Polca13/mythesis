d1d02422baaabc853e6c22175b3d9756065a7a3b
Polca13 <72781747+Polca13@users.noreply.github.com>
2021-10-19 17:25:39 +0800

    Add files via upload

---
+++ Pages/home.php
@@ -0,0 +1,40 @@
+<div class="home">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                <img src="img/coc.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Meet Our Team</h5>
+                    <p class="card-text">We are COC family, a group of students that dreamed to be successful someday</p>
+                </div>
+                <div class="card-footer">
+                <center><a href=".?folder=pages/&page=AboutUs" class="btn btn-primary">About Us</a></center>
+                </div>
+            </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="img/dev.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Meet our Developers</h5>
+                    <p class="card-text">A group of 4th year students from EVSU-OCC taking up BS in Information Technology.</p>
+                </div>
+                <div class="card-footer">
+                <center><a href=".?folder=pages/&page=Developers" class="btn btn-primary">Developers</a></center>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+                </div>
+            </div>
+        </div>
+</div>
